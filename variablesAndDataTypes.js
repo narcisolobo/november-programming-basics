6cbd6dc6cc6fd3598358646de7eb1c57f1b7a623
narcisolobo <narcisolobo@gmail.com>
2021-10-25 13:04:25 -0500

    first commit

---
+++ variablesAndDataTypes.js
@@ -0,0 +1,57 @@
+// Two forward slashes start a comment line
+// Mac - command + /
+// PC - ctrl + /
+
+// Variables
+// To create a variable, start with the 'var' keyword, name your variable (with a meaningful name), the assignment operator, and then the value.
+
+// Primitive DataTypes
+// Strings - strings of characters. They have quotation marks at their bookends.
+
+var faveColor = "green";
+var greeting = 'Hello, how are you?';
+var order = 'Don\'t swim in the pool!';
+var quote = "Ciso said, \"Always give meaningful names.\"";
+var firstName = "Narciso";
+var lastName = "Lobo";
+// adding strings together is called concatenation
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// Rules when naming variables - cannot start with a number, cannot contain a hyphen (dash), cannot contain any spaces
+
+// Guidelines - use camel case and meaningful names.
+
+// Numbers
+var age = 32;
+var temp = 45.7;
+
+// Arithmetic operators
+// +, -, *, /
+
+var ten = 10;
+var tenString = "10";
+
+var result = tenString + ten;
+console.log(result);
+
+// Booleans
+// true or false
+
+var isRaining = true;
+
+// Conditional operators
+// == equal to (only value)
+// === equal to (value and datatype)
+// < less than
+// > greater than
+// <= less than or equal to
+// >= greater than or equal to
+
+console.log(ten == tenString);
+// between the parentheses, we have a boolean expression, or a piece of code that will evaluate to a boolean (true or false)
+
+// null and undefined
+var nothing = null;
+var nada;
+
