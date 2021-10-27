9cdd864c5400252c723b0c51d6490bc307c712e4
narcisolobo <narcisolobo@gmail.com>
2021-10-27 14:34:12 -0500

    added 10/27 notes

--- w1d3/loopsRedux.js
+++ w1d3/loopsRedux.js
@@ -45,6 +45,18 @@ Fizz
 FizzBuzz
 */
 
+for(var i = 1; i <= 100; i++){
+  if(i % 3 == 0 && i % 5 == 0){
+    console.log("FizzBuzz")
+  } else if (i % 5 == 0) {
+    console.log("Buzz")
+  } else if (i % 3 == 0) {
+    console.log("Fizz")
+  } else {
+    console.log(i)
+  }
+}
+
 // While Loops
 
 // 1. Sentry
